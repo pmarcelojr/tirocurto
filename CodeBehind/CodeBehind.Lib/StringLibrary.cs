980d63164c666d00962877c99f1d2a4a3f176ee5
pmarcelojr <marcelosantostecnologia@gmail.com>
2022-10-20 13:27:51 -0300

    creating project library

---
+++ CodeBehind/CodeBehind.Lib/StringLibrary.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CodeBehind.Lib
+{
+    public static class StringLibrary
+    {
+        public static string Reverse(string s)
+        {
+            char[] charArray = s.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+
+        public static bool StartsWithUpper(this string? str)
+        {
+            if (string.IsNullOrWhiteSpace(str))
+                return false;
+
+            char ch = str[0];
+            return char.IsUpper(ch);
+        }
+    }
+}
