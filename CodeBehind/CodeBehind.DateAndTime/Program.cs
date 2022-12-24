097a1c701c7a5ad4ac1b2f9ea6165129c9d811e2
pmarcelojr <marcelosantostecnologia@gmail.com>
2022-12-24 16:48:12 -0300

    tiro curto about date and time in C#

---
+++ CodeBehind/CodeBehind.DateAndTime/Program.cs
@@ -0,0 +1,7 @@
+﻿DateTime dateTime = DateTime.Now;
+DateOnly dateOnly = DateOnly.FromDateTime(DateTime.Now);
+TimeOnly timeOnly = TimeOnly.FromDateTime(DateTime.Now);
+
+Console.WriteLine(dateTime);
+Console.WriteLine(dateOnly);
+Console.WriteLine(timeOnly);
